a3dadf911db9196441d5a4fd2511f605ee33995e
BombMarley <kile146@gmail.com>
2023-06-09 10:38:27 +0200

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,22 @@
+const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip 😍</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item.." />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/Stats.jsx
@@ -0,0 +1,9 @@
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>👜 You have x items on your list and you already packed X (X%)</em>
+    </footer>
+  );
+};
+
+export default Stats;
