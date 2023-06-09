01a056a4541701b7f8cf26dfb1c82ed8caa01708
BombMarley <kile146@gmail.com>
2023-06-09 11:05:13 +0200

    Added controlled elements

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,39 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity("");
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip 😍</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item.." />
+      <input
+        type="text"
+        placeholder="Item.."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button type="submit">Add</button>
     </form>
   );
